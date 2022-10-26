9ec64dc31a8f4df4e8681d3a48386763206b684b
devinchristianson <devin.christianson@maine.edu>
2022-10-25 20:10:23 -0400

    add final check logic for now

--- dynamicbeat/pkg/dynamicbeat/testchecks.go
+++ dynamicbeat/pkg/dynamicbeat/testchecks.go
@@ -15,7 +15,6 @@ import (
 // Run starts dynamicbeat.
 func TestChecks(f *checksource.Filesystem) error {
 	zap.S().Infof("dynamicbeat is running to test checks! Hit CTRL-C to stop it.")
-
 	// Set up CTRL+C handler
 	quit := make(chan os.Signal, 1)
 	signal.Notify(quit, os.Interrupt)
@@ -34,51 +33,68 @@ func TestChecks(f *checksource.Filesystem) error {
 
 	// Start publisher goroutine
 	results := make(chan check.Result)
-	published := make(chan uint64)
+	published := make(chan int)
 	go publishTestEvents(results, published)
 
 	var wg sync.WaitGroup
 
-	// Making quit handles in goroutine because we don't need the for look anymore (when running one round)
-	go func() {
-		<-quit
-		// Purposefully don't wait for checks.RunChecks goroutines to exit because that could take 30s
-		//wg.Wait()
+	// The logic for running a round is moved here so that it can be executed immediately as well as per the ticker
+	runRound := func() {
+		zap.S().Infof("Number of goroutines: %d", runtime.NumGoroutine())
+		zap.S().Infof("Starting a series of %d checks", len(defs))
+
+		// Start the goroutine
+		started := make(chan bool)
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			run.Round(defs, results, started)
+		}()
+
+		// Wait until all the checks have been started before we refresh
+		// the checks from Elasticsearch to make sure that we don't
+		// overwrite the check definitions while they're in use.
+		// TODO: determine if it's possible to overwrite the defs while
+		// they're in use by the above function
+		<-started
+		zap.S().Infof("Started series of checks")
+
+		// Update the check definitions for the next round
+		defs, err = f.LoadAll()
+		if err != nil {
+			zap.S().Warnf("Failed to update check definitions : %s", err)
+		}
+	}
+	cleanup := func() {
+		// Purposefully dont wait for checks.RunChecks goroutines to exit so the user doesn't have to wait up to 30s
+		// wg.Wait()
 
 		// Close the event publishing queue so the publishEvents goroutine will exit
 		close(results)
-
 		// Wait for all events to be published
-		<-published
+		exit := len(defs) - <-published
 		close(published)
-		os.Exit(1)
-	}()
-
-	// The logic for running a round is moved here so that it can be executed immediately as well as per the ticker
-	zap.S().Infof("Number of goroutines: %d", runtime.NumGoroutine())
-	zap.S().Infof("Starting a series of %d checks", len(defs))
-	// Start the goroutine
-	started := make(chan bool)
-	wg.Add(1)
+		os.Exit(exit)
+	}
 	go func() {
-		defer wg.Done()
-		run.Round(defs, results, started)
+		<-quit
+		cleanup()
 	}()
-	<-started
-	zap.S().Infof("Started series of checks")
+	runRound()
 	wg.Wait()
+	cleanup()
 	return nil
 }
 
-func publishTestEvents(results <-chan check.Result, out chan<- uint64) {
-	published := uint64(0)
+func publishTestEvents(results <-chan check.Result, out chan<- int) {
+	succeeded := 0
 	for result := range results {
-		passed := "failed"
-		if result.Passed {
-			passed = "passed"
+		passed := "passed"
+		if !result.Passed {
+			passed = "failed"
+			succeeded++
 		}
 		zap.S().Infof("Check %s for team %s %s: %s", result.Metadata.Name, result.Metadata.Group, passed, result.Message)
-		published++
 	}
-	out <- published
+	out <- succeeded
 }
