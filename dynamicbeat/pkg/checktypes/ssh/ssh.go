32f9e73076afde620bb63d5fbc81f67da9cbd21d
devinchristianson <devin.christianson@maine.edu>
2023-02-23 15:42:08 -0500

    add ability for SSH checks to use PrivKey instead of Password

--- dynamicbeat/pkg/checktypes/ssh/ssh.go
+++ dynamicbeat/pkg/checktypes/ssh/ssh.go
@@ -2,7 +2,10 @@ package ssh
 
 import (
 	"context"
+	"encoding/base64"
+	"encoding/pem"
 	"fmt"
+	"log"
 	"regexp"
 	"strconv"
 	"time"
@@ -16,9 +19,10 @@ import (
 // it implements the "check" interface
 type Definition struct {
 	Config       check.Config // generic metadata about the check
-	Host         string       `optiontype:"required"`                    // IP or hostname of the host to run the SSH check against
-	Username     string       `optiontype:"required"`                    // The user to login with over ssh
-	Password     string       `optiontype:"required"`                    // The password for the user that you wish to login with
+	Host         string       `optiontype:"required"` // IP or hostname of the host to run the SSH check against
+	Username     string       `optiontype:"required"` // The user to login with over ssh
+	Password     string       `optiontype:"optional"` // The password for the user that you wish to login with
+	PrivKey      string       `optiontype:"optional"`
 	Cmd          string       `optiontype:"required"`                    // The command to execute once ssh connection established
 	MatchContent string       `optiontype:"optional"`                    // Whether or not to match content like checking files
 	ContentRegex string       `optiontype:"optional" optiondefault:".*"` // Regex to match if reading a file
@@ -32,11 +36,34 @@ func (d *Definition) Run(ctx context.Context) check.Result {
 
 	// Config SSH client
 	// TODO: change timeout to be relative to the parent context's timeout
+	auth := []ssh.AuthMethod{}
+	if d.Password != "" {
+		auth = append(auth, ssh.Password(d.Password))
+	}
+	if d.PrivKey != "" {
+		// decode the provided key data
+		dataBytes, err := base64.StdEncoding.DecodeString(d.PrivKey)
+		if err != nil {
+			log.Fatalf("unable to parse private key base64: %v", err)
+		}
+		// rebuild into a PEM
+		block := &pem.Block{
+			Type:  "RSA PRIVATE KEY",
+			Bytes: dataBytes,
+		}
+		// encode the PEM to a byte string
+		encodedPEM := pem.EncodeToMemory(block)
+		// create a signer for the SSH client to use
+		signer, err := ssh.ParsePrivateKey(encodedPEM)
+		if err != nil {
+			log.Fatalf("unable to use parsed private key: %v", err)
+		}
+		// add public key auth as an auth method
+		auth = append(auth, ssh.PublicKeys(signer))
+	}
 	config := &ssh.ClientConfig{
-		User: d.Username,
-		Auth: []ssh.AuthMethod{
-			ssh.Password(d.Password),
-		},
+		User:            d.Username,
+		Auth:            auth,
 		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
 		Timeout:         20 * time.Second,
 	}
